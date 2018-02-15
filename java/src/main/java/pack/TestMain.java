648b79d87ef143bf282f2d92c8b31916ac0892b2
Igor Lifar <lifar@yandex-team.ru>
2018-02-16 01:10:08 +0300

    test threads

--- java/src/main/java/pack/TestMain.java
+++ java/src/main/java/pack/TestMain.java
@@ -1,10 +1,36 @@
 package pack;
 
+import java.util.ArrayList;
+
 /**
  * @author lifar
  */
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        ArrayList<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 16; i++) {
+            int finalI = i;
+            Thread thread = new Thread(() -> {
+                try {
+                    Thread.sleep(1000 * finalI);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(String.format("Thread #%d", finalI));
+            });
+            thread.start();
+            threads.add(thread);
+        }
+
+        threads.forEach(x -> {
+            try {
+                x.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
